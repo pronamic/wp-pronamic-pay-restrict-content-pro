a86e8c28fc6592c38e029de2f0e0f5160715ab1a
Reüel van der Steege <reuel@pronamic.nl>
2018-01-03 10:55:35 +0100

    Happy 2018.

--- src/DirectDebitSofortGateway.php
+++ src/DirectDebitSofortGateway.php
@@ -3,7 +3,7 @@
 /**
  * Title: Restrict Content Pro Direct Debit (mandate via Sofort) gateway
  * Description:
- * Copyright: Copyright (c) 2005 - 2017
+ * Copyright: Copyright (c) 2005 - 2018
  * Company: Pronamic
  *
  * @author Reüel van der Steege

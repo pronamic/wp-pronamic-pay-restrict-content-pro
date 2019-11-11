473de37b119cc6e544e55ee5f91d7b7db2aeb554
Reüel van der Steege <reuel@pronamic.nl>
2019-11-11 15:55:04 +0100

    Handle exception during payment start.

--- src/Gateway.php
+++ src/Gateway.php
@@ -232,11 +232,9 @@ class Gateway extends RCP_Payment_Gateway {
 		$payment->config_id = $config_id;
 		$payment->method    = $this->payment_method;
 
-		$payment = Plugin::start_payment( $payment, $gateway );
-
-		$error = $gateway->get_error();
-
-		if ( is_wp_error( $error ) ) {
+		try {
+			$payment = Plugin::start_payment( $payment, $gateway );
+		} catch ( \Exception $e ) {
 			do_action( 'rcp_registration_failed', $this );
 
 			wp_die(
@@ -244,7 +242,7 @@ class Gateway extends RCP_Payment_Gateway {
 					sprintf(
 						/* translators: %s: JSON encoded payment data */
 						__( 'Payment creation failed before sending buyer to the payment provider. Error: %s', 'pronamic_ideal' ),
-						$error->get_error_message()
+						$e->getMessage()
 					)
 				),
 				esc_html__( 'Payment Error', 'pronamic_ideal' ),

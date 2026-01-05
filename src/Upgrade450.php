e637572dbecb482594e5c0769829af791e94659b
Remco Tolsma <info@remcotolsma.nl>
2026-01-05 14:58:56 +0100

    2005-2026 Pronamic

--- src/Upgrade450.php
+++ src/Upgrade450.php
@@ -3,7 +3,7 @@
  * Upgrade 4.5.0
  *
  * @author    Pronamic <info@pronamic.eu>
- * @copyright 2005-2024 Pronamic
+ * @copyright 2005-2026 Pronamic
  * @license   GPL-3.0-or-later
  * @package   Pronamic\WordPress\Pay\Upgrades
  */

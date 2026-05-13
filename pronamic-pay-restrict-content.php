9b1bf9c7190c409a949647d1a3209a06fe3c568e
Remco Tolsma <info@remcotolsma.nl>
2026-05-13 11:11:34 +0200

    Use correct text domain for manual URL
    Update the i18n text domain for the Mollie integration manual URL from 'pronamic-pay-with-mollie-for-contact-form-7' to 'pronamic-pay-restrict-content' in pronamic-pay-restrict-content.php so translations are loaded for the correct plugin.

--- pronamic-pay-restrict-content.php
+++ pronamic-pay-restrict-content.php
@@ -68,7 +68,7 @@ add_filter(
 	function ( $gateways ) {
 		$gateways[] = new \Pronamic\WordPress\Pay\Gateways\Mollie\Integration(
 			[
-				'manual_url' => \__( 'https://www.pronamicpay.com/en/manuals/how-to-connect-mollie-to-wordpress-with-pronamic-pay/', 'pronamic-pay-with-mollie-for-contact-form-7' ),
+				'manual_url' => \__( 'https://www.pronamicpay.com/en/manuals/how-to-connect-mollie-to-wordpress-with-pronamic-pay/', 'pronamic-pay-restrict-content' ),
 			]
 		);
 
